180e8e871b9af36f358a7a34965a61af9a2fe957
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-30 20:40:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  def grade(grade)
+    roster[grade]
+  end
+  def sort 
+    roster.each do |grade, student|
+      roster[grade]= student.sort
+  end 
+roster
+  end
