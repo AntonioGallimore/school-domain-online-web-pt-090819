554a7e5c91d41083424efd7167a8af732bac4d98
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-30 21:13:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
     @roster = {}
   end
   
-  def add_student(name, grade)
+  def add_student(student_name, grade)
     if roster[grade]
       rooster[grade] << student_name
     else
